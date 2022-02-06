36da4a00ac3883bc0c21f6d1a012876531849020
Abner Santana <abnermuxah@gmail.com>
2022-02-06 06:43:31 -0300

    rotas URL

---
+++ agende/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path
+from agende.views import home, cadastro, agendamento
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', home),
+    path('cadastro/', cadastro),
+    path('agendamento/', agendamento)
+]
