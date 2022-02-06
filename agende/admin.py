a3d549928ea7bea43fb3852c15e922e08c6de013
Abner Santana <abnermuxah@gmail.com>
2022-02-06 15:36:35 -0300

    Armazenamento no BD
    USUARIO, UNIDADE< AGENDAMENTO

--- agende/admin.py
+++ agende/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import usuario
+from .models import agendamento, usuario, unidade
 # Register your models here.
 
 
@@ -7,4 +7,16 @@ class UsuarioAdmin(admin.ModelAdmin):
     ...
 
 
+class UnidadeAdmin(admin.ModelAdmin):
+    ...
+
+
+class AgendamentoAdmin(admin.ModelAdmin):
+    ...
+
+
 admin.site.register(usuario, UsuarioAdmin)
+
+admin.site.register(unidade, UnidadeAdmin)
+
+admin.site.register(agendamento, AgendamentoAdmin)
--- agende/models.py
+++ agende/models.py
@@ -1,6 +1,21 @@
+from email.policy import default
 from django.db import models
 from django.http import HttpResponse
 
 
 class usuario(models.Model):
-    nome_completo = models.CharField(max_length=60)
+    nome = models.CharField(max_length=65, default=None)
+    cpf = models.IntegerField(max_length=65, default=None)
+    data_nasc = models.DateField(default=None)
+    senha = models.CharField(max_length=12, default=None)
+
+
+class unidade (models.Model):
+    cod_unid = models.CharField(max_length=65, default=None)
+    nome_unid = models.CharField(max_length=65, default=None)
+
+
+class agendamento(models.Model):
+    data = models.DateTimeField()
+    cpf = models.ForeignKey(usuario, on_delete=models.CASCADE)
+    cod_und = models.ForeignKey(unidade, on_delete=models.CASCADE)
