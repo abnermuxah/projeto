0534670378563f09c57d6f02ebf0865db5d0fbf2
Abner Santana <abnermuxah@gmail.com>
2022-02-06 14:15:16 -0300

    criando modelos

--- agende/admin.py
+++ agende/admin.py
@@ -1,2 +1,10 @@
 from django.contrib import admin
+from .models import usuario
 # Register your models here.
+
+
+class UsuarioAdmin(admin.ModelAdmin):
+    ...
+
+
+admin.site.register(usuario, UsuarioAdmin)
--- agende/models.py
+++ agende/models.py
@@ -2,8 +2,5 @@ from django.db import models
 from django.http import HttpResponse
 
 
-class cadastro(models.Model):
+class usuario(models.Model):
     nome_completo = models.CharField(max_length=60)
-    nome_completo = models.CharField(max_length=60)
-    data_nasc = models.CharField(max_length=10)
-    senha = models.CharField(max_length=10)
